9d0ffd89b9772e16d642006fd693d3fd0f884509
Alexis Brandeker <alexis@astro.su.se>
2023-04-17 01:02:21 +0200

    Initial upload

---
+++ mock.py
@@ -0,0 +1,118 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 16 00:13:03 2023
+
+@author: Alexis Brandeker (alexis@astro.su.se)
+
+Photometric Optimal Extraction of Time Series Spectra (POETSS)
+
+This is a data simulator class, used to generate data to be used
+by POETSS
+
+
+"""
+import numpy as np
+
+
+class PoetssData():
+    """Simulates data to be used for POETSS
+    """
+    
+    def __init__(self, shape):
+        """shape is shape to be used for data cube
+        (#frames, #spatial pxiels, #wavelength pixels)
+
+        """
+        self.shape = shape
+        self.start_fwhm = 2     # FWHM of PSF at start column, in pixels
+        self.end_fwhm = 3       # FWHM of PSF at end column, in pixels
+        self.jitter_std = 0.01  # Jitter std between frames, in pixels
+        self.cr = 1e-3          # Cosmic Ray hit rate, per pixel
+        self.bad = 5e-4         # Fraction of bad pixels
+        self.bg_noise = 23     # Background noise, electrons per pixel
+        self.slope = -3.1       # Slope of trace over detector, in pixels
+        self.sed_fall = 0.5     # How much the SED falls to from the peak
+        self.signal = 1e5       # Max number of photons per column
+
+
+    def generate(self, occ_depth=100):
+        """Occultation depth in ppm, assumed to occur during 
+        central third of timeline
+        Returns simulated data cube
+        (frame number, spatial direction, wavelength direction)
+        """
+        x = np.arange(self.shape[1])
+        w = np.arange(self.shape[2])
+        
+        self.star_spec = self._spectrum(w)
+        self.planet_spec = self._spectrum(w) * occ_depth * 1e-6
+        self.occ_start = int(self.shape[0]/3)
+        self.occ_end = int(self.shape[0]*2/3)
+        self.dx = self.jitter_std * np.random.randn(self.shape[0])        
+        self.bad_map = np.random.rand(self.shape[1], self.shape[2]) <= self.bad
+
+        X = x[None,:,None] - self._trace(w)[None,None,:] - self.dx[:,None,None]
+        W = np.ones(self.shape)*w[None,None,:]
+        signal = self._psf(W, X)
+        signal[:self.occ_start] *= self.star_spec[None,None,:] + self.planet_spec[None,None,:]
+        signal[self.occ_start:self.occ_end] *= self.star_spec[None,None,:]
+        signal[self.occ_end:] *= self.star_spec[None,None,:] + self.planet_spec[None,None,:]
+        signal *= self.signal
+
+        variance = signal + self.bg_noise**2
+        data = signal + variance**.5 * np.random.randn(self.shape[0], self.shape[1], self.shape[2])
+        data[:,self.bad_map] = 0
+        hot_ind = np.random.rand(self.shape[0], self.shape[1], self.shape[2]) < self.cr
+        hot_amp = 1e4 + 1e6*np.random.rand(np.sum(hot_ind))
+        data[hot_ind] = hot_amp
+        
+        return data, variance**.5
+
+
+    def _psf(self, w, x):
+        """Returns the PSF at column w and spatial offset x from trace
+        centre. The PSF FWHM changes linearly with column coordinate
+        (wavelength)
+        """
+        self.fwhm_scale = (self.end_fwhm-self.start_fwhm)/self.shape[2]
+        s = (self.start_fwhm + w*self.fwhm_scale)/2.335
+        return np.exp(-0.5*x**2/s**2)/(s*(2*np.pi)**.5)
+
+
+    def _trace(self, w):
+        """Define the spectral trace as row coordinates for
+        input column coordinates. The trace is a quadratic
+        function of column (wavelength)
+        """
+        v = w/self.shape[2]
+        return 0.5*self.shape[1] + self.slope*(v-0.5) - v*(v-1)
+
+        
+    def _spectrum(self, w):
+        """Produce a spectrum with random absorption lines
+        and a baseline fall off with wavelength
+        """
+        v = w/self.shape[2]
+        
+        # Add absorption lines to spectrum
+        Nlines = int(self.shape[2]/10)
+        line_std = 0.5/Nlines
+        tau = np.zeros(self.shape[2])
+        max_tau = 0.3
+        for n in range(Nlines):
+            v0 = np.random.rand()
+            tau0 = max_tau*np.random.rand()
+            sigma = line_std*np.random.rand()
+            tau += tau0*np.exp(-0.5*(v-v0)**2/sigma**2)
+        
+        # Let SED baseline fall off linearly
+        baseline = 1 - (1-self.sed_fall)*v
+
+        # Assume spectral resolution to be 2 pixels and convolve
+        xx = np.linspace(-5,5,11)
+        kern = np.exp(-xx**2)
+        kern /= np.sum(kern)
+            
+        return np.convolve(baseline*np.exp(-tau), kern, mode='same')
+            
+        
---
+++ poetss.py
@@ -0,0 +1,289 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 16 10:42:52 2023
+
+@author: Alexis Brandeker (alexis@astro.su.se)
+
+Photometric Optimal Extraction of Time Series Spectra (POETSS)
+
+This is a collection of routines that optimally extracts photometry
+from spectral time series from space telescopes where the PSF and pointing
+are stable.
+
+These routines assume a basic reduction have been made and that the data 
+are on the following format:
+
+flux data in a cube with (frame#, row# (spatial direction), column# (wavelentgth direction))
+the corresponding noise cube, containing 1 std uncertainties for all data
+a bad pixelmap, i.e. a map of pixels that are bad in all frames. The format is
+a boolean fram where pixel is True if bad, False otherwise.
+
+The routines are then used to 
+
+1) Identify outliers (cosmic rays))
+2) Determine the trace of the spectrum in all frames, finding the relative
+    offset in the spatial direction (due to jitter)
+3) Shift the region around the trace into a matrix where the spectral trace
+    is approximately parallel to the rows
+4) Define linear correlation between pixel values and dx
+5) Extract photometry and error per column in frame
+
+"""
+import numpy as np
+import warnings
+
+
+def find_trace_cof(clean_cube, margin=5):
+    """ Use centre-of-flux to measure the trace for each frame and column.
+    margin is how many pixels outside of the trace should be considered.
+    """
+    cube = clean_cube.copy()
+    cube[cube<0] = 0 
+    cube /= np.maximum(1, np.max(cube, axis=1))[:,None,:]
+
+    border = int(cube.shape[2]/10)
+    m = np.median(cube, axis=0)
+    start_m = np.mean(m[:,:border], axis=1)
+    start_x = np.where(start_m == np.max(start_m))[0][0]
+    end_m = np.mean(m[:,-border:], axis=1)
+    end_x = np.where(end_m == np.max(end_m))[0][0]
+    dx = max(np.abs(end_x-start_x), margin)
+    x0 = max(int(min(start_x, end_x)-dx), 0)
+    x1 = min(int(max(start_x, end_x)+dx), cube.shape[1])
+    
+    # Compute centre of flux for each column
+    row = np.arange(cube.shape[0])
+    cent_mat = (np.sum(cube[:, x0:x1, :]*row[None, x0:x1, None], axis=1) / 
+              np.maximum(np.sum(cube[:,x0:x1,:], axis=1), 1e-5))
+    return cent_mat
+
+
+
+def fit_multi_trace(cent_mat, deg=2, clip=3):
+    """Fits a combined trace to the centers 
+    measured in cent_mat (#frame, #column)
+    deg is degree used in fitting polynomial,
+    clip is significance used for clipping
+    Returns trace (array with position for each column)
+    and dx, array with offsets per frame.
+    """
+    med_cent = np.median(cent_mat, axis=0)    
+    dx = np.median(cent_mat-med_cent[None,:], axis=1)
+    cent_cent = cent_mat - dx[:,None]
+    coeffs = np.zeros((len(cent_cent), deg+1))
+    
+    for n in range(len(cent_cent)):
+        coeffs[n] = fit_trace_poly(cent_cent[n], deg=deg, clip=clip)
+        
+    c = np.mean(coeffs, axis=0)
+    p = np.poly1d(c)
+    w = np.arange(cent_mat.shape[1])
+    trace = p(w)
+    dx = np.median(cent_mat-trace[None,:], axis=1)
+
+    return trace, dx
+
+
+def fit_trace_poly(cent_vec, deg=2, clip=3, niter=10):
+    """Fits a polynomial to the measured positions. Uses
+    sigma-clipping to remove outliers from fit
+    """
+    w = np.arange(len(cent_vec))
+    c = np.polyfit(w, cent_vec, deg=deg)
+    p = np.poly1d(c)
+    sel0 = np.ones(len(cent_vec), dtype='?')
+    for n in range(niter):
+        s = np.std(cent_vec[sel0]-p(w[sel0]))
+        sel1 = np.abs(cent_vec-p(w)) <= clip*s
+        c = np.polyfit(w[sel1], cent_vec[sel1], deg=deg)
+        if np.prod(sel0==sel1) == 1:
+            break
+        p = np.poly1d(c)
+        sel0 = sel1
+    return c
+    
+
+def cr2nan(data, bad_map, clip=5, niter=5):
+    """ 
+    Replaces outliers with NaN assuming there are only
+    statistical differenes between frames, and defines
+    anything more than clip std away per pixel as an outlier
+    
+    data is 3D array in format [frame#, row#, col#]
+    bad_map is 2D array in format [row#, col#]] where
+           True is bad pixel and False is good pixel
+    clip is how much a cvalue should deviate to be defined as an outlier
+    niter is the maxium number of iterations in sigma-clipping algorithm
+    """
+    indata = data.copy()
+    indata[:, bad_map] = np.nan
+    indata[0, bad_map] = 0      # To avoid pixels being NaN in all frames
+    nandata = indata.copy()
+    ind0 = np.zeros(indata.shape, dtype='?')
+    
+    for n in range(niter):
+        s = np.nanstd(nandata, axis=0)
+        m = np.nanmedian(nandata, axis=0)
+        nandata = indata.copy()
+        ind1 = np.abs(nandata-m) > clip*s
+        print('Iter {:d}/{:d} masked: {:d} = {:.2f}%'.format(n+1, niter,
+                    np.sum(ind1), 100.0*np.sum(ind1)/np.prod(data.shape)))
+        nandata[ind1] = np.nan
+        if n > 2 and np.prod(ind0 == ind1):
+            break
+        ind0 = ind1
+    nandata[0,bad_map] = np.nan
+
+    return nandata
+ 
+
+def clean_nan(nandata, max_iter=50, Nchunks=8):
+    """Replace NaN data points in a cube by interpolation. In a first step
+    avoid interpolating across rows (since flux gradients are 
+    generally greater there), but if any nan are left (can happen in case
+    of bad rows), interpolate along all axes for a maxiumum of max_iter
+    iterations.
+    To ease memory requirements, the data can be divided up in Nchunks
+    chunks.
+    """
+    clean = np.zeros_like(nandata)
+    chunk = np.array(np.linspace(0,len(clean), Nchunks+1), dtype=int)
+    
+    for n in range(len(chunk)-1):
+        clean[chunk[n]:chunk[(n+1)]] = replace_nan(nandata[chunk[n]:chunk[(n+1)]], max_iter=2, axis=(0,2))
+    if np.sum(np.isnan(clean)) == 0:
+        return clean
+    for n in range(len(chunk)-1):
+        clean[chunk[n]:chunk[(n+1)]] = replace_nan(clean[chunk[n]:chunk[(n+1)]], max_iter=max_iter)
+    return clean
+
+
+def extract_trace(data_cube, trace, psf_rad=5):
+    """Extracts a slit of radius psf_rad around the
+    trace in the data_cube, and returns a new cube
+    of same #frames and #columns, but with the slit length
+    # of rows.
+    """
+    slitlen = int(2*psf_rad+1)
+    x0 = np.array(trace-psf_rad, dtype=int)
+    x1 = x0 + slitlen
+    out_cube = np.zeros((data_cube.shape[0], slitlen, len(trace)))
+    for n in range(len(trace)):
+        out_cube[:, :, n] = data_cube[:, x0[n]:x1[n], n]
+    return out_cube
+
+
+def define_poly_coeff(clean_cut, dx, deg=1):
+    """For each pixel in the frame, this defines polynomial coefficients
+    (flux as a function of dx). Returns array of coefficients of 
+    format (#coeffs per poly, #rows, #cols)
+    """
+    coeffs = np.zeros((deg+1, clean_cut.shape[1], clean_cut.shape[2]))
+    
+    for n in range(clean_cut.shape[2]):    
+        coeffs[:, :, n] = np.polyfit(dx, clean_cut[:,:,n], deg=deg)
+    return coeffs
+
+
+def photometry(data, noise, poly_coeffs, dx):
+    """Performs photometry on data cube of format (frame#, row#, col#),
+    where chaning row changes spatial direction and changing column changes
+    wavelength.
+    The poly_coeffs are polynomial coefficients for each pixel in frame,
+    determining how the sensitivity changes with offset dx
+    dx is an array of 1D offset along spatial direction for all frames in
+    cube
+    Returns 
+    """
+    noise *= data/data  # This ensures that NaNs from data are present in noise
+    base = np.ones_like(data)
+    for n in range(data.shape[1]):
+        for m in range(data.shape[2]):
+            p = np.poly1d(coeffs[:,n,m])
+            base[:, n, m] = p(dx)
+    
+    weighted_data = data*base/noise**2
+    weights = (base/noise)**2
+    weight_sum = np.nansum(weights, axis=1)
+
+    lightcurve = np.nansum(weighted_data, axis=1)/weight_sum
+    lightcurve_err = 1/weight_sum**.5    
+    synthspec = base*lightcurve[:,None,:]
+    
+    return lightcurve, lightcurve_err, synthspec
+
+
+# Tools
+
+def replace_nan(data, max_iter=50, axis=None):
+    """Replaces NaN-entries by mean of neighbours.
+    Iterates until all NaN-entries are replaced or
+    max_iter is reached. Works on N-dimensional arrays.
+    If axis tuple is defined, interpolations are made only
+    along those axes.
+    Memory intensive for large cubes.
+    """
+    if axis is None:
+        axis = tuple(range(data.ndim))
+    nan_data = data.copy()
+    shape = np.append([2*len(axis)], data.shape)
+    interp_cube = np.zeros(shape)
+    shift0 = np.zeros(len(axis), int)
+    shift0[0] = 1
+    shift = []
+    for n in range(len(axis)):
+        shift.append(tuple(np.roll(-shift0, n)))
+        shift.append(tuple(np.roll(shift0, n)))
+    for _j in range(max_iter):
+        for n in range(2*len(axis)):
+            interp_cube[n] = np.roll(nan_data, shift[n], axis=axis)
+        with warnings.catch_warnings():
+            warnings.simplefilter('ignore', RuntimeWarning)
+            mean_data = np.nanmean(interp_cube, axis=0)
+        nan_data[np.isnan(nan_data)] = mean_data[np.isnan(nan_data)]
+        if np.sum(np.isnan(nan_data)) == 0:
+            break
+    return nan_data
+
+
+if __name__ == '__main__': 
+    import mock
+    from time import time
+    T0 = T1 = time()
+    
+    def timeit(label=''):
+        global T0, T1
+        T = time()
+        print('{:s}: T = {:.3f} s, DT = {:.3f} s'.format(label, T-T0, T-T1))
+        T1 = T
+    
+    shape = (16100, 64, 1000)
+    pd = mock.PoetssData(shape)
+    timeit('mock')
+
+    data, noise = pd.generate(occ_depth=100)
+    timeit('Generate data')
+    
+    nandata = cr2nan(data, pd.bad_map)
+    timeit('Find NaN')
+
+    clean = clean_nan(nandata)
+    timeit('Clean NaN')
+
+    cent_mat = find_trace_cof(clean)
+    timeit('Trace measurement')
+    
+    trace, dx = fit_multi_trace(cent_mat)
+    timeit('Trace fit')
+    
+    clean_cut = extract_trace(clean, trace, 5)
+    data_cut = extract_trace(nandata, trace, 5)
+    noise_cut = extract_trace(noise, trace, 5)
+    timeit('Extract trace')
+    
+    coeffs = define_poly_coeff(clean_cut, dx, deg=1)
+    timeit('Fitting coeffs')
+
+    lc, lc_err, synth, ff, ff_err = photometry(data_cut, noise_cut, coeffs, dx)
+    timeit('Photometry')
+    
